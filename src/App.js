fdef7f30441298bca674ef3057c95b50baee106e
Jose <j.a.ojeda@live.com>
2020-08-02 21:07:15 -0700

    imported ArticleProvider into App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import Articles from './containers/Articles'
+import React from 'react';
+import ArticleProvider from "./context/articleContext";
+import Articles from './containers/Articles';
 import AddArticle from "./components/AddArticle/AddArticle";
 
 
 function App() {
   return (
-    <>
+    <ArticleProvider>
        <AddArticle />
        <Articles />
-    </>
+    </ArticleProvider>
     )
 }
 export default App
