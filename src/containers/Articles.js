3ecb831f6dc170c46fa0edb8b8f9cd13a7dd64b7
Jose <j.a.ojeda@live.com>
2020-08-02 21:13:28 -0700

    updated Articles comp to use context

--- src/containers/Articles.js
+++ src/containers/Articles.js
@@ -1,19 +1,18 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
+import { ArticleContext } from '../context/articleContext';
 import Article from '../components/Article/Article';
 
 const Articles = () => {
-  const [articles, setArticles] = useState([
-    { id: 1, title: "post 1", body: "Quisque cursus, metus vitae pharetra" },
-    { id: 2, title: "post 2", body: "Quisque cursus, metus vitae pharetra" },
-  ])
+  
+  const { articles } = useContext(ArticleContext);
 
-return (
-    <div>
-      {articles.map(article => (
-        <Article key={article.id} article={article} />
-      ))}
-    </div>
-  )
+  return (
+      <div>
+        {articles.map(article => (
+          <Article key={article.id} article={article} />
+        ))}
+      </div>
+    )
 }
 
 export default Articles
